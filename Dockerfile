64af1ffd3f2f0c60c272ae0e58f81c3fbeaad22a
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-04-09 23:12:39 +0200

    added docker and kubernetes related stuff

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:15.13-alpine3.13
 
+RUN apk --no-cache add curl
+
 WORKDIR /dummy-app
 COPY . .
 RUN mkdir volume
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const fs = require('fs');
 require('dotenv');
-const app = require('express')();
+const express = require('express');
+const app = express();
+const { join } = require('path');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(
+  join(__dirname, '/var/lib/sqlite/db/db.db'),
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+  }
+);
 const { default: axios } = require('axios');
 
 const PORT = process.env.PORT || 3000;
 const APP_NAME = process.env.APP_NAME || 'dummy-app';
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.method, req.originalUrl);
+  next();
+});
+
 app.get('/health', (req, res) => {
-  const date = Date.now();
-  fs.writeFileSync(`./volume/${date}`, `${date}`, { encoding: 'utf-8' });
+  console.log('health - DONE');
   res.send({ status: APP_NAME });
 });
 
@@ -19,10 +38,46 @@ app.get('/deep-health', async (req, res) => {
     const { data: notificationsHealth } = await axios.get(
       `${process.env.NOTIFICATIONS_URL}/health`
     );
+    console.log('deep-health - DONE');
     res.send({ postsHealth, notificationsHealth });
   } catch (error) {
+    console.log(error);
     res.send(error);
   }
+});
+
+app.get('/posts', (req, res, next) => {
+  let tableFound = true;
+  db.run('select * from posts', (err) => {
+    tableFound = false;
+  });
+
+  const sql = `select * from posts`;
+  let results = {};
+  db.run(sql, [], (err, rows) => {
+    if (!tableFound) {
+      db.run('create table posts (id varchar(20) primary key, caption text)');
+      return res.send({});
+    }
+    if (err) {
+      console.log(err);
+      return res.send({ err });
+    }
+    rows.forEach((row, i) => {
+      results[i] = row.caption;
+    });
+    res.send({ results });
+  });
+});
+
+app.post('/posts', (req, res, next) => {
+  let tableFound = true;
+  db.run('select * from posts', (err) => (tableFound = false));
+  if (!tableFound) {
+    db.run('create table posts (id varchar(20) primary key, caption text)');
+  }
+  db.run(`insert into posts (caption) values ("${req.body.caption}")`);
+  res.send();
 });
 
 app.listen(PORT, () => {
